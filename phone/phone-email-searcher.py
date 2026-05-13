669b2c559c2719534f3fcec92865adc437b2ef4d
jchandd <jaydenc243@deltalearns.ca>
2026-05-13 10:00:30 -0700

    addded initial pseudocode

---
+++ phone/phone-email-searcher.py
@@ -0,0 +1,23 @@
+#Phone and Email Searcher
+# This program will search a peice of text you get from clipboard and parse it for phone numbers and email adresses.
+# It will put only phone numbers and email addresses it found into the clipboard
+# While doing this project make sure to use regular expressions to find the phone numbers and email addresses
+# You can use the re module in python to work with regular expressions
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
