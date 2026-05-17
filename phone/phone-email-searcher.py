06ae83a873f4b3b30f0d0e579ccf12cf467eddec
jchandd <jaydenc243@deltalearns.ca>
2026-05-17 11:31:48 -0700

    added the phone number searcher

--- phone/phone-email-searcher.py
+++ phone/phone-email-searcher.py
@@ -4,9 +4,20 @@
 # While doing this project make sure to use regular expressions to find the phone numbers and email addresses
 # You can use the re module in python to work with regular expressions
 
+import pyperclip
+import re
 
-
-
+# Phone Number Regex
+phoneRegex = re.compile(r'''
+(
+    (\d{3}|\(\d{3}\))?      # area code
+    (\s|-|\.)?              # separator
+    (\d{3})                 # first 3 digits
+    (\s|-|\.)               # separator
+    (\d{4})                 # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
+)
+''', re.VERBOSE)
 
 
 
