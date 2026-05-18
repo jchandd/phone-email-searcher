4e6d404c79c97febf5b998285df963d546182419
jchandd <jaydenc243@deltalearns.ca>
2026-05-18 11:14:12 -0700

    added large parts of the phone and email searcher

--- phone/phone-email-searcher.py
+++ phone/phone-email-searcher.py
@@ -19,18 +19,28 @@ phoneRegex = re.compile(r'''
 )
 ''', re.VERBOSE)
 
+# Email regex
+emailRegex = re.compile(r'''
+(
+    [a-zA-Z0-9._%+-]+       # username
+    @                       # @ symbol
+    [a-zA-Z0-9.-]+          # domain name
+    (\.[a-zA-Z]{2,4})       # dot-something
+)
+''', re.VERBOSE)
 
+# Find matches in clipboard text
+text = str(pyperclip.paste())
 
+matches = []
 
+# Find phone numbers
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
 
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
 
-
-
-
-
-
-
-
-
+    matches.append(phoneNum)
 
 
