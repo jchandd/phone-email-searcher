a712e0733be565c07e71ef83cdb11d51d982933e
jchandd <jaydenc243@deltalearns.ca>
2026-05-19 10:26:13 -0700

    cutt and added stuff

--- phone/phone-email-searcher.py
+++ phone/phone-email-searcher.py
@@ -6,39 +6,25 @@
 
 import re
 import pyperclip
-# Get the text from the clipboard
-text = pyperclip.paste()
-# Create a regex for phone numbers
-phoneRegex = re.compile(r'''(
-    (\d{3}|\(\d{3}\))?              # area code
-    (\s|-|\.)?                       # separator
-    (\d{3})                           # first 3 digits
-    (\s|-|\.)                         # separator
-    (\d{4})                           # last 4 digits
-    (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension
-)''', re.VERBOSE)               
-# Create a regex for email addresses
-emailRegex = re.compile(r'''(
-    [a-zA-Z0-9._%+-]+      # username
-    @                       # @ symbol
-    [a-zA-Z0-9.-]+         # domain name
-    (\.[a-zA-Z]{2,4})      # dot-something
-)''', re.VERBOSE)  
-# Find all the matches in the text and put them in a list
-matches = []for groups in phoneRegex.findall(text):
-    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
-    if groups[8] != '':
-        phoneNum += ' x' + groups[8]
-    matches.append(phoneNum)
-for groups in emailRegex.findall(text):
-    matches.append(groups[0])
-# Copy the results to the clipboard
-if len(matches) > 0:
-    pyperclip.copy('\n'.join(matches))
-    print('Copied to clipboard:')
-    print('\n'.join(matches))
-else:
-    print('No phone numbers or email addresses found.')
+
+
+def find_vin_numbers():
+    # Get text from clipboard
+    text = pyperclip.paste()
+
+    # Define regex pattern for VIN numbers
+    vin_regex = re.compile(r'\b[A-HJ-NPR-Z\d]{17}\b')
+
+    # Find all matches of VIN numbers in text
+    vin_numbers = vin_regex.findall(text)
+
+    # Return list of VIN numbers
+    return vin_numbers
+
+
+print(find_vin_numbers())
+
+
 
 
 
